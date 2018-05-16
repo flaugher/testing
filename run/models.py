970c048e8c373b8f9fafa4e366514bf5a9cfe030
Robert Flaugher <rlfprog@gmail.com>
2018-05-16 16:19:40 -0700

    Add Author, Book, and Publisher models

--- run/models.py
+++ run/models.py
@@ -1,5 +1,50 @@
 from django.db import models
 
+"""
+Each author publishes one or more books.
+Each book has one and only one author.
+Each author publishes with one or more publishers.
+Each publisher publishes one or more authors.
+"""
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'author'
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'book'
+
+class Publisher(models.Model):
+    """
+    a = Author(name='author_name')
+    a.save()
+    p = Publisher(house='house_name')
+    p.save()
+    p.authors.add(a)
+    """
+    house = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.house
+
+    class Meta:
+        db_table = 'publisher'
+
+
+
+
 class Car(models.Model):
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
